83e6637b9403d9ac6ad8bead622ec54c0cbd6a6b
Samantha Traiman <samantha.traiman+git@gmail.com>
2023-01-11 17:12:01 +0200

    added routes to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import Header from './components/header.js';
 import './App.css';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/products" element={<Gallery />}>
+            <Route path=":id" element={<Product />} />
+          </Route>
+          <Route path="/category/:category" element={<Gallery />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/orders" element={<Orders />}>
+            <Route path=":id" element={<Cart />} />
+          </Route>
+          <Route path="/contact-us" element={<Contact />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
