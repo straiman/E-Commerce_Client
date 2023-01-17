7f4e85697595face95dc7badd0ecb46f2d66aff8
Samantha Traiman <samantha.traiman+git@gmail.com>
2023-01-17 10:38:47 +0200

    Added basic bootstrap login form

--- src/components/login.js
+++ src/components/login.js
@@ -1,8 +1,25 @@
 import React from 'react';
 
-import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Button from 'react-bootstrap/Button';
 
 function Login(props) {
-    return <Container></Container>;
+    return <Form>
+        <FloatingLabel
+            controlID="floatingInput"
+            label="Email address"
+            classname="mb-3">
+                <Form.Control type="email" placeholder="name@example.com" />
+        </FloatingLabel>
+        <FloatingLabel
+            controlID="floatingPassword"
+            label="Password">
+                <Form.Control type="password" placeholder="Password" />
+        </FloatingLabel>
+        <Button variant="primary" type="button">
+            Log In
+        </Button>
+    </Form>;
 }
 export default Login;
